cfd28d3440228e96a725cbb524d96577c2a4efd7
csillagkrisztian <krisztian.csillag93@gmail.com>
2021-08-16 20:36:22 +0200

    idemo

---
+++ Javascript/lecke2/lecke2.js
@@ -0,0 +1,20 @@
+const korhazBajmok = require('../../database');
+
+// kondiciok, funkciok, ciklus
+const a = 5;
+const b = 3;
+let nagyAzA;
+//1   2     3
+if (a>b) {
+    nagyAzA=true
+// 4      5
+} else { /*opcionalis*/ 
+    nagyAzA=false
+}
+
+// 1 if - ha
+// 2 kondicio - feltetel
+// 3 kod aminek futnia kell
+// 4 else - ha nem
+// 5 kod aminek futnia kell
+
