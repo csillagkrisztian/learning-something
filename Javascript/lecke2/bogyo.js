2e43ec02f277cd0f9a2be658597feecb2e3c863b
bogyo49 <88431223+bogyo49@users.noreply.github.com>
2021-08-17 20:49:39 +0200

    probalkozok

--- Javascript/lecke2/bogyo.js
+++ Javascript/lecke2/bogyo.js
@@ -1,3 +1,6 @@
 const korhazunk = require("../../database");
 
-console.log(korhazunk.doktorok[]);
+//console.log(korhazunk.doktorok[0].paciensek[1].suly);
+
+console.log(korhazunk.doktorok[1].paciensek[0].magassag);
+
