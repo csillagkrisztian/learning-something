cdac243747870f145cfa7e8d2946d2ea0f1f537b
bogyo49 <88431223+bogyo49@users.noreply.github.com>
2021-08-18 11:01:18 +0200

    bogyoahazi

--- Javascript/lecke2/bogyo.js
+++
@@ -1,6 +0,0 @@
-const korhazunk = require("../../database");
-
-//console.log(korhazunk.doktorok[0].paciensek[1].suly);
-
-console.log(korhazunk.doktorok[1].paciensek[0].magassag);
-
---
+++ Javascript/lecke2/bogyohazi.js
@@ -0,0 +1,69 @@
+// Database
+const korhazunk = require("../../database");
+
+// Define docs and patients
+
+const drName1 = [korhazunk.doktorok[0].nev];
+const drName2 = [korhazunk.doktorok[1].nev];
+const p1Name = [korhazunk.doktorok[0].paciensek[0].nev];
+const p1Age = [korhazunk.doktorok[0].paciensek[0].kor];
+const p2Name = [korhazunk.doktorok[0].paciensek[1].nev];
+const p2Age = [korhazunk.doktorok[0].paciensek[1].kor];
+const p3Name = [korhazunk.doktorok[1].paciensek[0].nev];
+const p3Age = [korhazunk.doktorok[1].paciensek[0].kor];
+const p4Name = [korhazunk.doktorok[1].paciensek[1].nev];
+const p4Age = [korhazunk.doktorok[1].paciensek[1].kor];
+
+// Define insurance
+
+let insHaveOrNot = Boolean(korhazunk.doktorok[0].paciensek[0].insurance);
+let insHaveOrNot2 = Boolean(korhazunk.doktorok[0].paciensek[1].insurance);
+let insHaveOrNot3 = Boolean(korhazunk.doktorok[1].paciensek[0].insurance);
+let insHaveOrNot4 = Boolean(korhazunk.doktorok[1].paciensek[1].insurance);
+
+// Define text strings for the senctences
+const a = "A ";
+const nameOf = " nevű doktor ";
+const nameOfP = " nevű páciense ";
+const ageOf = " éves";
+const andHave = " és van érvényes biztosítása.";
+const andNotHave = " és nincs érvényes biztosítása.";
+
+// Main Function
+
+if (insHaveOrNot) {
+  console.log(
+    a + drName1 + nameOf + p1Name + nameOfP + p1Age + ageOf + andHave
+  );
+} else {
+  console.log(
+    a + drName1 + nameOf + p1Name + nameOfP + p1Age + ageOf + andNotHave
+  );
+}
+if (insHaveOrNot2) {
+  console.log(
+    a + drName1 + nameOf + p2Name + nameOfP + p2Age + ageOf + andHave
+  );
+} else {
+  console.log(
+    a + drName1 + nameOf + p2Name + nameOfP + p2Age + ageOf + andNotHave
+  );
+}
+if (insHaveOrNot3) {
+  console.log(
+    a + drName2 + nameOf + p3Name + nameOfP + p3Age + ageOf + andHave
+  );
+} else {
+  console.log(
+    a + drName1 + nameOf + p3Name + nameOfP + p3Age + ageOf + andNotHave
+  );
+}
+if (insHaveOrNot4) {
+  console.log(
+    a + drName2 + nameOf + p4Name + nameOfP + p4Age + ageOf + andHave
+  );
+} else {
+  console.log(
+    a + drName1 + nameOf + p4Name + nameOfP + p4Age + ageOf + andNotHave
+  );
+}
