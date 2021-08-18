42accbe42cc43793ff84e28d41c681e446a377ed
bogyo49 <88431223+bogyo49@users.noreply.github.com>
2021-08-18 11:04:07 +0200

    hazi

--- database.js
+++ database.js
@@ -1,55 +1,55 @@
+const suly = "kg"
+const magassag = "cm"
 const paciens1 = {
-  nev: "Csilla",
-  kor: 25,
-  magassag: 168 + "cm",
-  suly: 57 + "kg",
-  id: 1,
+    nev: "Csilla",
+    kor: 25,
+    magassag: 168 + magassag,
+    suly: 57 + suly,
+    id: 1,
+    insurance: true,
 };
 
 const paciens2 = {
-  nev: "Viola",
-  kor: 40,
-  magassag: 165 + "cm",
-  suly: 65 + "kg",
-  id: 2,
+    nev: "Viola",
+    kor: 40,
+    magassag: 165 + magassag,
+    suly: 65 + suly,
+    id: 2, 
+    insurance: false,
 };
 
 const paciens3 = {
-  nev: "Gyuri",
-  kor: 48,
-  magassag: 186 + "cm",
-  suly: 97 + "kg",
-  id: 3,
+    nev: "Gyuri",
+    kor: 48,
+    magassag: 186 + magassag,
+    suly: 97 + suly,
+    id: 3,
+    insurance: true,
 };
 
 const paciens4 = {
-  nev: "Viktor",
-  kor: 30,
-  magassag: 180 + "cm",
-  suly: 102 + "kg",
-  id: 4,
+    nev: "Viktor",
+    kor: 30,
+    magassag: 180 + magassag,
+    suly: 102 + suly,
+    id: 4,
+    insurance: false,
 };
 
 const doktor1 = {
-  nev: "Bertalan",
-  paciensek: [paciens1, paciens2],
-};
+    nev: "Bertalan",
+    paciensek: [paciens1,paciens2]
+}
 
 const doktor2 = {
-  nev: "Jozsef",
-  paciensek: [paciens3, paciens4],
-};
+    nev: "Jozsef",
+    paciensek: [paciens3,paciens4]
+}
 
 const korhazBajmok = {
-  nev: "St' Bajmok",
-  doktorok: [doktor1, doktor2],
-};
-
-//milyen nehez vagy?[   / kg/lbs/]
-const kg = "kg";
-const lbs = "lbs";
-let felhasznaloBevitle = 150;
-let kivalasztottMertekegyseg = kg;
+    nev: "St' Bajmok", 
+    doktorok:[doktor1, doktor2]
+}
 
 //m
 module.exports = korhazBajmok;
