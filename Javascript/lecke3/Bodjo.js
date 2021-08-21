7a9a3da53995a791cbb493e840e3a561a55f6911
bogyo49 <88431223+bogyo49@users.noreply.github.com>
2021-08-21 18:14:33 +0200

    első kor

--- Javascript/lecke3/Bodjo.js
+++ Javascript/lecke3/Bodjo.js
@@ -1,13 +1,13 @@
 const korhazunk = require("../../database");
 
+
+
 const doktorok =korhazunk.doktorok;
-
-console.log(doktorok);
-const current = doktorok[0]
-
-if(current.nev[0] === "B" && current.paciensek[1].nev[0] === "V") {   // A STRING AZ EGY LISTAAAAA!
-    console.log("BIZONY")
-}else{
-    console.log("nem ám")
-}
+const patientList =[
+korhazunk.doktorok[0].paciensek[0],
+korhazunk.doktorok[0].paciensek[1],
+korhazunk.doktorok[1].paciensek[0],
+korhazunk.doktorok[1].paciensek[1],
+]
+console.log(patientList)
 
