70ad92f7dfe090a2ccae51432954c64f476f8d0c
Schloder <86109796+Schloder@users.noreply.github.com>
2021-08-23 22:30:27 +0200

    Gyogyszer

--- Javascript/lecke3/schloderr.js
+++ Javascript/lecke3/schloderr.js
@@ -1,6 +1,5 @@
 const korhazunk = require("../../database");
 
-
 const pac = [...korhazunk.doktorok[0].paciensek, ...korhazunk.doktorok[1].paciensek];
 
 
@@ -13,13 +12,14 @@ for  (i = 0; i < pac.length; i++){
   let kg1 = parseInt(kg);
   let mag1 = parseInt(mag);
 
-  
     if (mag1> 175 && kg1 < 70 && kor <30 ) {
       console.log(nev + ':[Bensedin, Brufen]');
   } else if (mag1 > 175 && kg1 > 100 && bizt === true) {
     console.log(nev + ':[Glucophage, Brufen]');  
   } else if (mag1 > 175 && kor > 60 && bizt === true) {
-    console.log(nev + ':[Diclophen, Brufen]');  
+    console.log(nev + ':[Diclophen, Brufen]');
+  } else if(kg1 > 100 && kor > 60 && bizt === true){
+    console.log(nev + ':[Glucophage, Diclophen]');    
   } else if (kg1 < 70 && kor < 30 ) {
     console.log(nev + ':[Bensedin]');
   } else if (kor > 60 && bizt === true) {
@@ -29,5 +29,5 @@ for  (i = 0; i < pac.length; i++){
   } else if (mag1> 175) {
       console.log(nev + ':[Brufen]');
   } else 
-    console.log(nev + ':[null]')
-}
+    console.log(nev + ':[null]');
+};
