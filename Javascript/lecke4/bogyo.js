710f066844f765d8fcd06a930417b648a01b1329
Norbert B <bnorbertb@gmail.com>
2021-08-26 22:11:30 +0200

    pffff

--- Javascript/lecke4/bogyo.js
+++ Javascript/lecke4/bogyo.js
@@ -1,4 +1,4 @@
-const a = 420;
+/*const a = 420;
 const b = 69;
 
 function ajde(first, second) {
@@ -7,3 +7,14 @@ function ajde(first, second) {
 const result = ajde(a, b);
 console.log(result);
 //console.log(ajde(a, b));
+*/
+
+let myList = ["pizza", "dzsuvety", "tortilla"];
+
+function finom(kaja) {
+    const text = "A kaják amiket szeretek:"
+    for (let i = 0; i < kaja.length; i++)
+  
+}
+return "A kaják amiket szeretek:" + ;
+console.log(finom(myList));
