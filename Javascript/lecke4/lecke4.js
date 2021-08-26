dc3d5d550d90bc73b6358bd093b8df999e6c79f3
csillagkrisztian <krisztian.csillag93@gmail.com>
2021-08-26 20:38:59 +0200

    tessek

---
+++ Javascript/lecke4/lecke4.js
@@ -0,0 +1,29 @@
+// Funkciok
+// 1        2     3
+function funkcio(arg) {
+  //   4
+  // [...]
+  // 5    6
+  return arg;
+}
+//7     8         9      10
+const valami = funkcio("valami");
+/* 
+1. function - definicio
+2. funkcio - nev
+3. arg - argumentum/ mindegy mi a neve
+4. [...] - barmilyen kod
+5. return - visszahozasa az adatnak/opcionalis
+6. arg - az adat amit visszahozunk
+7. const - varians definicio
+8. valami - az a varians ahol taroljuk a visszahozott adatot
+9. funckio - funckionknak az "instantiate" inditoja
+10. valami - adat amit a funckionak adunk, hogy az argumentuma legyen
+*/
+
+const a = 5;
+const b = 3;
+
+function osszeadas(first, second) {
+  return first + second;
+}
