0f1cfbf2ce059a2de41f1101de0a5f5481f15530
Schloder <86109796+Schloder@users.noreply.github.com>
2021-08-31 19:37:25 +0200

    blabal

--- Javascript/lecke3/schloderr.js
+++ Javascript/lecke3/schloderr.js
@@ -1,9 +1,19 @@
 const korhazunk = require("../../database");
 
-const pac = [...korhazunk.doktorok[0].paciensek, ...korhazunk.doktorok[1].paciensek];
+const pac = [];
 
+for (let i = 0; i < korhazunk.doktorok.length; i++) {
+  pac.push(...korhazunk.doktorok[i].paciensek);
+}
 
-for  (i = 0; i < pac.length; i++){
+let gyogyszerObjektum = {};
+
+const BRU = "Brufen";
+const GLU = "Glucopage";
+const DIC = "Diclophen";
+const BEN = "Bensedin";
+
+for (i = 0; i < pac.length; i++) {
   let nev = pac[i].nev;
   let kor = pac[i].kor;
   let kg = pac[i].suly;
@@ -12,22 +22,24 @@ for  (i = 0; i < pac.length; i++){
   let kg1 = parseInt(kg);
   let mag1 = parseInt(mag);
 
-    if (mag1> 175 && kg1 < 70 && kor <30 ) {
-      console.log(nev + ':[Bensedin, Brufen]');
-  } else if (mag1 > 175 && kg1 > 100 && bizt === true) {
-    console.log(nev + ':[Glucophage, Brufen]');  
-  } else if (mag1 > 175 && kor > 60 && bizt === true) {
-    console.log(nev + ':[Diclophen, Brufen]');
-  } else if(kg1 > 100 && kor > 60 && bizt === true){
-    console.log(nev + ':[Glucophage, Diclophen]');    
-  } else if (kg1 < 70 && kor < 30 ) {
-    console.log(nev + ':[Bensedin]');
-  } else if (kor > 60 && bizt === true) {
-    console.log(nev + ':[Diclophen]');
-  } else if (kg1 > 100 && bizt === true) {
-    console.log(nev + ':[Glucophage]');  
-  } else if (mag1> 175) {
-      console.log(nev + ':[Brufen]');
-  } else 
-    console.log(nev + ':[null]');
-};
+  const gyogyszer = [];
+
+  if (mag1 > 175) {
+    gyogyszer.push(BRU);
+  }
+  if (kg1 < 70 && kor < 30) {
+    gyogyszer.push(BEN);
+  }
+  if (kg1 > 100 && bizt === true) {
+    gyogyszer.push(GLU);
+  }
+  if (kor > 60 && bizt === true) {
+    gyogyszer.push(DIC);
+  }
+  if (gyogyszer.length === 0) {
+    gyogyszer.push("null");
+  }
+  gyogyszerObjektum[nev] = gyogyszer;
+}
+
+console.log(gyogyszerObjektum);
